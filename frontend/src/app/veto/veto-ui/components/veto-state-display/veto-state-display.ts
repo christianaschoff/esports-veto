a01ccd759c5f46d3fb1c87c9c047178da37b2626
Christian Aschoff <christian.aschoff@diw-sc.de>
2026-01-02 18:50:53 +0100

    flip flop veto and pick phase

--- frontend/src/app/veto/veto-ui/components/veto-state-display/veto-state-display.ts
+++ frontend/src/app/veto/veto-ui/components/veto-state-display/veto-state-display.ts
@@ -1,6 +1,8 @@
 import { Component, computed, inject } from '@angular/core';
-import { VetoState } from '../../../../data/veto.data';
+import { VetoState, VetoStepType } from '../../../../data/veto.data';
 import { VetoStore } from '../../../../store/veto-store';
+import { VetoConstellationService } from '../../../../services/veto-constellation.service';
+import { VETO_ACTION } from '../../../../data/veto-constellation.data';
 
 @Component({
   selector: 'app-veto-state-display',
@@ -11,11 +13,35 @@ import { VetoStore } from '../../../../store/veto-store';
 export class VetoStateDisplay {
 
   state = inject(VetoStore);    
+  vetoConstellationService = inject(VetoConstellationService);
   VetoStateEnum=VetoState;
 
   currentState = computed(() => {  
     return this.state.currentGameState().vetoState as VetoState;
   });
+
+  isVeto = computed(() => 
+  {
+    const vetoSteps = this.state.currentGameState().vetoSteps;
+    const nextStepIndex = vetoSteps.findIndex(step => step.stepType === VetoStepType.Unset);
+    if (nextStepIndex === -1) return false; 
+
+    // this is a little hack
+    const constellation = {
+      bestOf: this.state.bestOf(),
+      playerA: this.state.playerA(),
+      playerB: this.state.playerB(),
+      vetoSystem: this.state.vetoSystem(),
+      gameid: this.state.gameId(),
+      mode: this.state.modus(),
+      maps: this.state.maps()
+    };
+    const rounds = this.vetoConstellationService.calculate(constellation);
+    const nextRound = rounds[nextStepIndex];
+  
+    return nextRound?.actionType === VETO_ACTION.VETO;
+  });
+  
 
   amIPlayerA = computed(() => {
     return this.state.attendee.userId() === this.state.currentGameState().vetoSteps[0].playerId;
